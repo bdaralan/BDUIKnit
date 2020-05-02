6ed48f6feb252fbb711c9e6f29a23223d530341d
Dara <bdaralan@gmail.com>
2020-05-01 21:16:30 -0700

    Add size to BDModalDragHandle constructor

--- Sources/BDUIKnit/InternalResuableView/BDModalDragHandle.swift
+++ Sources/BDUIKnit/InternalResuableView/BDModalDragHandle.swift
@@ -8,24 +8,31 @@
 import SwiftUI
 
 
+/// A horizontal handle bar.
+///
+/// A handle bar that is normally sit on top of a view which indicates that the view is draggable.
+///
 public struct BDModalDragHandle: View {
     
     @Environment(\.verticalSizeClass) private var verticalSizeClass
+    
+    var size: CGSize
     
     var color: Color
     
     var hideOnVerticalCompact: Bool
     
     
-    public init(color: Color = .primary, hideOnVerticalCompact: Bool = false) {
+    public init(size: CGSize = .init(width: 50, height: 4), color: Color = .primary, hideOnVerticalCompact: Bool = false) {
+        self.size = size
         self.color = color
         self.hideOnVerticalCompact = hideOnVerticalCompact
     }
     
     
     public var body: some View {
-        RoundedRectangle(cornerRadius: 2, style: .continuous)
-            .frame(width: 50, height: 4, alignment: .center)
+        RoundedRectangle(cornerRadius: size.height / 2, style: .continuous)
+            .frame(width: size.width, height: size.height, alignment: .center)
             .foregroundColor(color)
             .opacity((hideOnVerticalCompact && verticalSizeClass == .compact) ? 0 : 1)
     }
